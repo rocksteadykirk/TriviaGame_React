6f65d9ce396041f748ae68fca2f19bef78feae43
Samantha Johnson <samantha.nisus@gmail.com>
2023-10-29 20:55:21 -0500

    objective page set with footer

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer className='site-footer'>
+            &copy; <a href='https://github.com/SamJohn87' target='_blank'>Samantha Nisus-Johnson</a>
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ src/components/GameObjective.js
@@ -0,0 +1,17 @@
+import { Container, Row, Col, Button } from "reactstrap";
+
+const GameObjective = () => {
+    return (
+        <>
+            <div className='welcome'><p>Welcome to DiceQuizz Trivia</p></div>
+            <div className='objective'>
+                <p>The objective of the game is to answer trivia questions correctly and earn points.</p>
+                <p>The sum of the two dice rolled not only determines the points but also the time available to answer each question.</p>
+                <p>The player with the most points at the end wins.</p>
+                <Button className='btn-objective'>Let's Go!</Button>
+            </div >
+        </>
+    );
+}
+
+export default GameObjective;
