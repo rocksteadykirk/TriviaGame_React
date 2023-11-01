32a806770cfc592587d92020afdc5af7f6aab7c6
Kirk Goulbourne <kpgoulbourne@gmail.com>
2023-11-01 10:18:39 +0900

    added kirks name to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <footer className='site-footer'>
-            &copy; <a href='https://github.com/SamJohn87' target='_blank'>Samantha Nisus-Johnson</a>
+            &copy; <a href='https://github.com/SamJohn87' target='_blank'>Samantha Nisus-Johnson</a> & <a href='https://github.com/rocksteadykirk' target='_blank'>Kirk Goulbourne</a>
         </footer>
     );
 };
