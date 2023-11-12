a7dbdbd11c51891441c981541dcbad38def0c35e
Samantha Johnson <samantha.nisus@gmail.com>
2023-11-12 15:06:54 -0600

    add state management with useReducer hook

--- src/pages/GameBoard.js
+++ src/pages/GameBoard.js
@@ -1,10 +1,18 @@
+import { useReducer } from "react";
+import { initialState } from "../features/playersState";
+import { listReducer } from "../features/playersState";
 import PlayerNameForm from "../features/PlayerNameForm";
+import PlayersList from "../features/PlayersList";
 
 const GameBoard = () => {
+    const [state, dispatch] = useReducer(listReducer, initialState);
+    const { listPlayers } = state;
+
     return (
         <>
             <h1>You are seeing the GameBoard component</h1>
-            <PlayerNameForm />
+            <PlayerNameForm dispatch={dispatch} />
+            <PlayersList listPlayers={listPlayers} />
         </>
     );
 };
