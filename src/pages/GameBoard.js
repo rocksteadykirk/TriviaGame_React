34e9f58a3c1b06f4aa6cf64e6e1bc635adce6142
Samantha Johnson <samantha.nisus@gmail.com>
2023-11-13 20:13:12 -0600

    added dice component with roll dice button and 2 dices

--- src/pages/GameBoard.js
+++ src/pages/GameBoard.js
@@ -3,6 +3,7 @@ import { initialState } from "../features/playersState";
 import { listReducer } from "../features/playersState";
 import PlayerNameForm from "../features/PlayerNameForm";
 import PlayersList from "../features/PlayersList";
+import Dice from "../features/Dice";
 
 const GameBoard = () => {
     const [state, dispatch] = useReducer(listReducer, initialState);
@@ -12,6 +13,7 @@ const GameBoard = () => {
             <h1>You are seeing the GameBoard component</h1>
             <PlayerNameForm dispatch={dispatch} />
             <PlayersList listPlayers={state} />
+            <Dice />
         </>
     );
 };
